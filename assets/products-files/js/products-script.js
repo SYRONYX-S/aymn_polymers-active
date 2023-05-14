7a870aec5fd235b96d1cc5defa74d703e9abb3e2
hashim <hashimshafeeque.m@gmail.com>
2023-05-14 17:42:07 +0530

    updates

--- assets/products-files/js/products-script.js
+++ assets/products-files/js/products-script.js
@@ -1,13 +1,16 @@
 var swiper = new Swiper('.blog-slider', {
-      spaceBetween: 30,
-      effect: 'fade',
-      loop: true,
-      mousewheel: {
-        invert: false,
-      },
-      // autoHeight: true,
-      pagination: {
-        el: '.blog-slider__pagination',
-        clickable: true,
-      }
-    });
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 4000, // set the delay between slides in milliseconds
+    disableOnInteraction: false // allow user interaction with the slider
+  }
+});
